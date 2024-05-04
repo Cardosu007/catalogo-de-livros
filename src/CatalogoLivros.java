e5d63285aa288fd6575a7ce46b5f853427480aa1
W Cardoso <cardosu7@gmail.com>
2024-05-04 11:40:24 -0300

    criando catalogo de livros

---
+++ src/CatalogoLivros.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class CatalogoLivros {
+
+
+    private List<Livro> livroList;
+
+    public CatalogoLivros(){
+        this.livroList = new ArrayList<>();
+    }
+
+    public void adicionarLivro(String titulo, String autor, int anoPubli){
+
+    }
+
+    public void pesquisarPorAutor(String autor){
+
+    }
+
+    public void pesquisarPorIntervaloAnos(int anoInicial, int anoFinal){
+        
+    }
+}
